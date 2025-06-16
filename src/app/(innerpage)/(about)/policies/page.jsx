56f673c0b697f11ebe0ec25c4f21be9fdd2e5785
wasif <wasifaleem2@gmail.com>
2025-06-16 17:58:56 +0500

    create a policy page with nothing right noew

---
+++ src/app/(innerpage)/(about)/policies/page.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import PageHeading from "@/app/Components/PageHeading";
+import Section from "@/app/Components/Section";
+
+const headingData = {
+  title: "Policies",
+};
+
+const Page = () => {
+  return (
+    <div>
+      <Section
+        className={"cs_page_heading cs_bg_filed cs_center"}
+        // backgroundImage="/assets/img/page_heading_bg.jpg"
+      >
+        <PageHeading data={headingData} />
+      </Section>
+
+      <Section
+        topSpaceLg="80"
+        topSpaceMd="120"
+        bottomSpaceLg="80"
+        bottomSpaceMd="120"
+      >
+      </Section>
+    </div>
+  );
+};
+
+export default Page;
