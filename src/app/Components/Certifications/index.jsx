4b66e6fe59f7b792df5d6742b12fe9504cf41c34
wasifaleem2 <wasifaleem2@gmail.com>
2025-12-08 13:51:55 +0500

    feat: changes

--- src/app/Components/Certifications/index.jsx
+++ src/app/Components/Certifications/index.jsx
@@ -7,17 +7,17 @@ const Certifications = () => {
       id: 1,
       title: "Shariah Compliance Certificate For Zakat",
       images: [
-        "/assets/img/certificates/Shariah Compliance Certificate For Zakat.jpg",
+        "/assets/img/certificates/Shariah Compliance Certificate for Zakat valid till 11.10.2026 (1)_page-0001.jpg",
       ],
       description: "Certification for Zakat collection and distribution according to Islamic principles."
     },
     {
       id: 2,
-      title: "FBR Tax Exemption Certificate (U/S 236-C)",
+      title: "FBR Tax Exemption Certificate 2025-2026",
       images: [
-        "/assets/img/certificates/FBR Tax Exemption Certificate.jpeg",
+        "/assets/img/certificates/HR&RT Tax Exemption Cert U-S 2-36-C valid till 30.06.26.jpg",
       ],
-      description: "Federal Board of Revenue tax exemption certification for donations."
+      description: "Federal Board of Revenue tax exemption certification for 2025-2026."
     },
     {
       id: 3,
@@ -28,15 +28,15 @@ const Certifications = () => {
       ],
       description: "Primary Care Partnership certification for healthcare standards."
     },
-    {
-      id: 4,
-      title: "FBR",
-      images: [
-        "/assets/img/certificates/FBR.jpeg",
-        "/assets/img/certificates/FBR-2.jpg"
-      ],
-      description: "Official registration with the Sindh Healthcare Commission."
-    },
+    // {
+    //   id: 4,
+    //   title: "FBR",
+    //   images: [
+    //     "/assets/img/certificates/FBR.jpeg",
+    //     "/assets/img/certificates/FBR-2.jpg"
+    //   ],
+    //   description: "Official registration with the Sindh Healthcare Commission."
+    // },
     {
       id: 3,
       title: "PCP",
@@ -49,7 +49,7 @@ const Certifications = () => {
       id: 4,
       title: "SHCH Registration",
       images: [
-        "/assets/img/certificates/SHCH Registration.png"
+        "/assets/img/certificates/Registration Certificate from Sindh Healthcare Commission-Revised.JPG"
       ],
       description: "Official registration with the Sindh Healthcare Commission."
     },
